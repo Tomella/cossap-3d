a1d0186a066806fdfd458dd52ce3af4ad75f26f2
Larry Hill <tomella1@gmail.com>
2017-03-09 15:53:50 +1100

    Tweak config and add topo image width

--- source/app/config.ts
+++ source/app/config.ts
@@ -2,6 +2,7 @@ declare var Elevation;
 
 export class Config {
    static preferences = {
+      surfaceWorkerLocation: "workers/target/broker.js",
       showGrid: true,
       showBoreholes: true,
       scaling: 1,
@@ -11,13 +12,14 @@ export class Config {
             name: "Imagery over Victoria",
          },
          template: "http://services.ga.gov.au/site_9/services/DEM_SRTM_1Second_over_Bathymetry_Topography/MapServer/WCSServer?SERVICE=WCS&VERSION=1.0.0&REQUEST=GetCoverage" +
-         "&coverage=1&CRS=EPSG:3857&BBOX=${bbox}&FORMAT=GeoTIFF&RESX=${resx}&RESY=${resy}&RESPONSE_CRS=EPSG:3857&HEIGHT=${height}&WIDTH=${width}",
+                     "&coverage=1&CRS=EPSG:3857&BBOX=${bbox}&FORMAT=GeoTIFF&RESX=${resx}&RESY=${resy}&RESPONSE_CRS=EPSG:3857&HEIGHT=${height}&WIDTH=${width}",
          esriTemplate: "http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/export?bbox=${bbox}&f=${format}&format=jpg&size=${size}",
          topoTemplate: "http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/export?bbox=${bbox}&f=image&format=jpg&size=${width},${height}",
-         resolutionX: 60,
+         resolutionX: 75,
          imageWidth: 256,
-         hiResX: 600,
+         hiResX: 700,
          hiResImageWidth: 2048,
+         hiResTopoWidth: 512,
          opacity: 1,
          extent: new Elevation.Extent2d(1000000, -10000000, 20000000, -899000),
       },
