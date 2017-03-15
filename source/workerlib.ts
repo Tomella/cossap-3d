35216fde00419bfabae2421c3cadd943945ea86c
Larry Hill <tomella1@gmail.com>
2017-03-16 10:59:01 +1100

    Add particles worker code to worker lib

--- source/workerlib.ts
+++ source/workerlib.ts
@@ -1,3 +1,4 @@
 export { SurfaceWorker } from "./workers/target/surfaceworker";
+export { ParticlesWorker } from "./workers/target/particlesworker";
 export { WorkerEvent } from "./workers/workerevent";
 export { Config } from "./app/config";
