ca23a380ccc56d82391e0bfc1c07205f65c1d058
Larry Hill <tomella1@gmail.com>
2017-03-16 10:52:34 +1100

    Worker support for rock particles

--- source/workers/workerevent.ts
+++ source/workers/workerevent.ts
@@ -4,4 +4,7 @@ export class WorkerEvent {
 
    static COLOR_LOADED = "color.loaded";
    static COLOR_BLOCK  = "color.block";
+
+   static PARTICLES_LOADED = "particles.loaded";
+   static PARTICLES_COMPLETE = "particles.complete";
 }
